42b95c538f3233b99cfea52f05701685f2d4275e
tienpvse1 <tienpvse@fpt.edu.vn>
2022-01-25 20:25:15 +0700

    add configuration

--- client/src/page/home.tsx
+++ client/src/page/home.tsx
@@ -1,14 +1,12 @@
 import axios from "axios";
-
 export const Home = ({}) => {
   const handleClick = async () => {
-    const { data } = await axios.get("/api/");
+    const { data } = await axios.get("api/");
     alert(data.message);
   };
 
   return (
     <div>
-      <h1>This is home</h1>
       <button onClick={handleClick}>get data</button>
     </div>
   );
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../be/client",
+    outDir: "../crm/public",
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost/",
+        changeOrigin: true,
+      },
+    },
   },
 });
