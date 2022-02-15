b28c7875eeca4848f62f687bb074580a27713c64
tienpvse1 <tienpvse@fpt.edu.vn>
2022-02-15 14:42:34 +0700

    resolved(profile)

--- client/src/components/header/menu-profile.tsx
+++ client/src/components/header/menu-profile.tsx
@@ -1,8 +1,7 @@
-import { Avatar, Dropdown } from 'antd';
-import { useLiveQuery } from 'dexie-react-hooks';
-import { MenuProfile } from './dropdown/dropdown-profile';
-import { useCookies } from 'react-cookie';
 import { PUBLIC_USER_INFO } from '@constance/cookie';
+import { Avatar, Dropdown } from 'antd';
+import { useCookies } from 'react-cookie';
+import { MenuProfile } from './dropdown/dropdown-profile';
 
 export const DropdownProfile = () => {
   const [cookies] = useCookies([PUBLIC_USER_INFO]);
@@ -13,7 +12,11 @@ export const DropdownProfile = () => {
     <Dropdown overlay={<MenuProfile />} trigger={['click']}>
       <div className='ant-dropdown-link'>
         <Avatar
-          src={publicInfo.photo}
+          src={
+            publicInfo.photo
+              ? publicInfo.photo
+              : 'https://buildyourspechere.com/wp-content/uploads/2020/10/placeholder-image-person-jpg-340x281.jpg'
+          }
           size={30}
           style={{ marginBottom: '8px' }}
         />
