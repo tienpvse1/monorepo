b7303a2fdbfd98e1d7673087ed7b3ab10cbf3a23
tienpvse1 <tienpvse@fpt.edu.vn>
2022-02-16 12:35:19 +0700

    resolved(fullstack): change lottie files location

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,27 +4,13 @@ import { route } from './routes/route-map';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './constance/color';
 import { Suspense } from 'react';
-import Loading from 'react-loading';
+import { Loading } from '@components/loading/loading';
 export const client = new QueryClient();
 function App() {
   const elements = useRoutes(route);
   return (
     <QueryClientProvider client={client}>
-      <Suspense
-        fallback={
-          <div
-            style={{
-              height: '100vh',
-              width: '100vw',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <Loading type='spin' color='red' height={75} width={75} />
-          </div>
-        }
-      >
+      <Suspense fallback={<Loading />}>
         <div className='App'>{elements}</div>
       </Suspense>
     </QueryClientProvider>
--- client/src/axios/index.ts
+++ client/src/axios/index.ts
@@ -26,3 +26,5 @@ export class Axios {
     );
   }
 }
+
+export const { instance } = new Axios();
