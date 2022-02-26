d65201a336a081d33fccc136d5a1aece890a49aa
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-02-26 21:44:43 +0700

    update interface contact

--- client/src/modules/contact/entity/contact.entity.ts
+++ client/src/modules/contact/entity/contact.entity.ts
@@ -2,10 +2,30 @@ import { IBase } from '@interfaces/base';
 
 export interface IContact extends IBase {
   name: string;
+  addresses: Address[];
+  noteWorthies: NoteWorthy[];
   phone: string;
-  address: string;
   type: string;
-  birth: string;
   email: string;
+  birth: string;
+  mobile: string;
+  photo: string;
+  state: string;
+  postalCode: string;
+  taxId: string;
+  jobPosition: string;
+  website: string;
+  title: string;
   internalNotes: string;
+}
+export interface Address {
+	type: string;
+	address: string;
+	city: string;
+	country: string;
+}
+
+export interface NoteWorthy {
+	name: string;
+	date: string;
 }
