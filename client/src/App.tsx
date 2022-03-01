e66f9ab15f01ac67fc5b8d9b5c72220d7532d233
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-01 12:51:36 +0700

    resolved(fe): switch to pnpm

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,22 @@
 import { Loading } from '@components/loading/loading';
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useRoutes } from 'react-router-dom';
 import './constance/color';
 import { route } from './routes/route-map';
 import './stylesheets/App.scss';
+import { io } from 'socket.io-client';
+import { envVars } from '@env/var.env';
 export const client = new QueryClient();
+const socket = io(`${envVars.VITE_BE_DOMAIN}/pipeline`);
 function App() {
   const elements = useRoutes(route);
+
+  useEffect(() => {
+    socket.on('pipeline-updated', (data) => {
+      console.log(data);
+    });
+  }, [socket]);
 
   return (
     <QueryClientProvider client={client}>
