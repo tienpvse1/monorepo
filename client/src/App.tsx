85970adf856dac5a571aff84746a825266d9e74c
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-01 13:13:08 +0700

    resolved(layout): add experimental notification

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,14 +7,36 @@ import { route } from './routes/route-map';
 import './stylesheets/App.scss';
 import { io } from 'socket.io-client';
 import { envVars } from '@env/var.env';
+import { Button, notification } from 'antd';
+import { IPipeline } from '@modules/pipeline/entity/pipeline.entity';
 export const client = new QueryClient();
 const socket = io(`${envVars.VITE_BE_DOMAIN}/pipeline`);
 function App() {
   const elements = useRoutes(route);
 
+  const openNotification = (data: IPipeline) => {
+    const key = `open${Date.now()}`;
+    const btn = (
+      <Button
+        type='primary'
+        size='small'
+        onClick={() => notification.close(key)}
+      >
+        Confirm
+      </Button>
+    );
+    notification.open({
+      message: 'Pipeline updated',
+      description: `${data.name} has been updated`,
+      btn,
+      key,
+      onClose: close,
+    });
+  };
+
   useEffect(() => {
-    socket.on('pipeline-updated', (data) => {
-      console.log(data);
+    socket.on('pipeline-updated', (data: IPipeline) => {
+      openNotification(data);
     });
   }, [socket]);
 
