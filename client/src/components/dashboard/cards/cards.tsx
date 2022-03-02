072ed086c205efa0f0759857eaeb1d83773659d0
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-02 15:14:43 +0700

    resolved(dashboard): add framer motion on dashboard component

--- client/src/components/dashboard/cards/cards.tsx
+++ client/src/components/dashboard/cards/cards.tsx
@@ -1,29 +1,74 @@
-import { Col, Row } from 'antd';
-import { controllers } from '../../../constance/controllers';
-import { CardMoney, CardOrders, CardSale, CardTicket } from './card';
+import { Row } from 'antd';
+import { Reorder } from 'framer-motion';
+import { useState } from 'react';
+import { CardItem } from './card/card-item';
 
-const { SIZE_CARD, SIZE_OFFSET } = controllers;
+export interface CardItemProps {
+  title?: string;
+  countUpEnd: number;
+  rate: number;
+  updateStatus: string;
+}
+const data: CardItemProps[] = [
+  {
+    title: 'Available Money',
+    countUpEnd: 25,
+    rate: 8700,
+    updateStatus: 'since last week',
+  },
+  {
+    title: 'Total order',
+    countUpEnd: 25,
+    rate: 8700,
+    updateStatus: 'since last week',
+  },
+  {
+    title: 'Total sales',
+    countUpEnd: 14,
+    rate: 8700,
+    updateStatus: 'since last week',
+  },
+  {
+    title: 'Available ticket',
+    countUpEnd: 25,
+    rate: 1200,
+    updateStatus: 'since last week',
+  },
+];
 
 export const CardBoard = () => {
-    const SIZE_GUTTER = 18;
-    return (
-        <>
-            <div className="site-card-wrapper">
-                <Row gutter={SIZE_GUTTER}>
-                    <Col span={SIZE_CARD} offset={SIZE_OFFSET}>
-                        <CardMoney />
-                    </Col>
-                    <Col span={SIZE_CARD}>
-                        <CardOrders />
-                    </Col>
-                    <Col span={SIZE_CARD}>
-                        <CardSale />
-                    </Col>
-                    <Col span={SIZE_CARD}>
-                        <CardTicket />
-                    </Col>
-                </Row>
-            </div>
-        </>
-    )
-}
+  const [cards, setCards] = useState<(CardItemProps & { index: number })[]>(
+    data.map((item, index) => ({ ...item, index }))
+  );
+  const SIZE_GUTTER = 18;
+  return (
+    <>
+      <div className='site-card-wrapper'>
+        <Row gutter={SIZE_GUTTER}>
+          <Reorder.Group
+            axis='x'
+            values={cards}
+            style={{ display: 'flex' }}
+            onReorder={setCards}
+          >
+            {cards.map((card, index) => (
+              <Reorder.Item
+                style={{ width: '15vw', marginLeft: index === 0 ? '0' : '4vw' }}
+                key={card.index}
+                value={card}
+                as='div'
+              >
+                <CardItem
+                  countUpEnd={card.countUpEnd}
+                  rate={card.rate}
+                  updateStatus={card.updateStatus}
+                  title={card.title}
+                />
+              </Reorder.Item>
+            ))}
+          </Reorder.Group>
+        </Row>
+      </div>
+    </>
+  );
+};
