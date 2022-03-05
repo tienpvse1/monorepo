b9d0cc9919f4953e0916c5e590c754e1099ec7cc
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-05 12:39:04 +0700

    resolved(api): add transaction to change stage api

--- crm/src/modules/pipeline-module/pipeline-item/pipeline-item.service.ts
+++ crm/src/modules/pipeline-module/pipeline-item/pipeline-item.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BaseService } from 'src/base/nestjsx.service';
-import { getCustomRepository, Repository } from 'typeorm';
+import { getConnection, getCustomRepository, Repository } from 'typeorm';
 import { PipelineColumnRepository } from '../pipeline-column/pipeline-column.repository';
 import { ChangeStageDto } from './dto/update-pipeline-item.dto';
 import { PipelineItem } from './entities/pipeline-item.entity';
@@ -19,44 +19,61 @@ export class PipelineItemService extends BaseService<PipelineItem> {
     const pipelineColumnRepository = getCustomRepository(
       PipelineColumnRepository,
     );
-    const oldPipelineColumn = await pipelineColumnRepository.findOneItem({
-      where: { id: dto.oldStageId },
-      relations: ['pipelineItems'],
-    });
-    const newPipelineColumn = await pipelineColumnRepository.findOneItem({
-      where: { id: dto.newStageId },
-      relations: ['pipelineItems'],
-    });
+
+    const [oldPipelineColumn, newPipelineColumn] = await Promise.all([
+      pipelineColumnRepository.findOneItem({
+        where: { id: dto.oldStageId },
+        relations: ['pipelineItems'],
+      }),
+      pipelineColumnRepository.findOneItem({
+        where: { id: dto.newStageId },
+        relations: ['pipelineItems'],
+      }),
+    ]);
 
     const itemToChange = oldPipelineColumn.pipelineItems.filter(
       (item) => item.id === id,
     )[0];
-    if (!itemToChange)
-      throw new BadRequestException(
-        'item with this id does not exist in provided old column ',
-      );
+    const connection = getConnection();
+    const queryRunner = connection.createQueryRunner();
+    await queryRunner.connect();
 
-    oldPipelineColumn.pipelineItems = oldPipelineColumn.pipelineItems.filter(
-      (item) => item.id !== id,
-    );
-    oldPipelineColumn.save();
-    if (dto.index != undefined) {
-      newPipelineColumn.pipelineItems.splice(dto.index, 0, itemToChange);
+    // lets now open a new transaction:
+    await queryRunner.startTransaction();
+    try {
+      if (!itemToChange)
+        throw new BadRequestException(
+          'item with this id does not exist in provided old column ',
+        );
 
-      newPipelineColumn.pipelineItems = newPipelineColumn.pipelineItems.map(
-        (item, index) =>
-          ({
-            ...item,
-            index,
-          } as PipelineItem),
+      oldPipelineColumn.pipelineItems = oldPipelineColumn.pipelineItems.filter(
+        (item) => item.id !== id,
       );
-    } else {
-      newPipelineColumn.pipelineItems.push({
-        ...itemToChange,
-        index: newPipelineColumn.pipelineItems.length,
-      } as PipelineItem);
+      queryRunner.manager.save(oldPipelineColumn);
+      if (dto.index != undefined) {
+        newPipelineColumn.pipelineItems.splice(dto.index, 0, itemToChange);
+
+        newPipelineColumn.pipelineItems = newPipelineColumn.pipelineItems.map(
+          (item, index) =>
+            ({
+              ...item,
+              index,
+            } as PipelineItem),
+        );
+      } else {
+        newPipelineColumn.pipelineItems.push({
+          ...itemToChange,
+          index: newPipelineColumn.pipelineItems.length,
+        } as PipelineItem);
+      }
+      // return newPipelineColumn.save();
+      const result = await queryRunner.manager.save(newPipelineColumn);
+      return result;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw new BadRequestException('create failure');
+    } finally {
+      await queryRunner.release();
     }
-    // return newPipelineColumn.save();
-    return newPipelineColumn.save();
   }
 }
