e9c79385247c515e98a497918c4aaf37c2095511
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-07 11:27:31 +0700

    resolved(layout): check contact information before insert

--- client/src/pages/import-contact.tsx
+++ client/src/pages/import-contact.tsx
@@ -1,20 +1,27 @@
 import { lazy, Suspense, useState } from 'react';
 import Upload from '@common/upload';
 import { Loading } from '@components/loading/loading';
+import { useContacts } from '@modules/contact/query/contact.get';
 const PreviewContactTable = lazy(
   () => import('@components/import-contact/preview-contact-table')
 );
 import { CreateContactDto } from '@modules/contact/dto/create-contact.dto';
 
 const ImportContact: React.FC = () => {
-  const [contacts, setContacts] = useState<CreateContactDto[]>([]);
+  const [importedContacts, setImportedContacts] = useState<CreateContactDto[]>(
+    []
+  );
+  const { data: contacts } = useContacts();
   return (
     <div>
-      {contacts.length <= 0 ? (
-        <Upload setContacts={setContacts} />
+      {importedContacts.length <= 0 ? (
+        <Upload setImportedContacts={setImportedContacts} contacts={contacts} />
       ) : (
         <Suspense fallback={<Loading />}>
-          <PreviewContactTable contacts={contacts} setContacts={setContacts} />
+          <PreviewContactTable
+            contacts={importedContacts}
+            setContacts={setImportedContacts}
+          />
         </Suspense>
       )}
     </div>
--- client/src/util/excel.ts
+++ client/src/util/excel.ts
@@ -1,13 +1,23 @@
 import { CreateContactDto } from '@modules/contact/dto/create-contact.dto';
 import { RcFile } from 'antd/lib/upload';
+import { nanoid } from 'nanoid';
 import { read, utils } from 'xlsx';
 
 export const readExcel = async (file: File | RcFile) => {
   const data = await file.arrayBuffer();
   const workbook = read(data);
   const sheets = workbook.SheetNames;
-  var xlData = utils.sheet_to_json<CreateContactDto>(
+  const xlData = utils.sheet_to_json<CreateContactDto & { __rowNum__: number }>(
     workbook.Sheets[sheets[0]]
   );
-  return xlData;
+  console.log(xlData);
+
+  const mapped = xlData.map((data) => ({
+    ...data,
+    id: nanoid(),
+    __rowNum__: data.__rowNum__ + 1,
+  }));
+  console.log(mapped);
+
+  return mapped;
 };
