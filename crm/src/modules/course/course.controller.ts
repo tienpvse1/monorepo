18bf6c71829ad46ec06b570c5429c5389c9e25ad
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-15 10:08:57 +0700

    resolved(api): fix product api with validations

--- crm/src/modules/course/course.controller.ts
+++ crm/src/modules/course/course.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Delete, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Post,
+  UsePipes,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Crud } from '@nestjsx/crud';
 import { HistoryLog } from 'src/common/decorators/message.decorator';
@@ -6,7 +13,7 @@ import { CourseService } from './course.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { Course } from './entities/course.entity';
-
+import { CreateCoursePipe } from './pipe/create-course.pipe';
 @Controller('course')
 @Crud({
   model: {
@@ -17,7 +24,7 @@ import { Course } from './entities/course.entity';
     update: UpdateCourseDto,
   },
   routes: {
-    exclude: ['deleteOneBase'],
+    exclude: ['deleteOneBase', 'createOneBase'],
   },
   query: {
     join: {
@@ -40,5 +47,12 @@ export class CourseController {
   @HistoryLog('deleted a course')
   delete(@Param('id') id: string) {
     return this.service.softDelete(id);
+  }
+
+  @Post()
+  @HistoryLog('created a course')
+  @UsePipes(CreateCoursePipe)
+  create(@Body() dto: CreateCourseDto) {
+    return this.service.createItem(dto);
   }
 }
