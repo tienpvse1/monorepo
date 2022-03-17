860d159603ece5523de57c661eb71ecac27e5a52
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-17 18:55:34 +0700

    resolved(layout): resolved import contact bug

--- client/src/modules/contact/dto/create-contact.dto.ts
+++ client/src/modules/contact/dto/create-contact.dto.ts
@@ -1,4 +1,21 @@
-import { IContact } from '../entity/contact.entity';
-export interface CreateContactDto extends Partial<IContact> {
+export interface IImportedContact {
+  name: string;
+  company: string;
+  phone: string;
+  type: string;
+  email: string;
+  jobPosition: string;
+  address: string;
+  internalNotes: string;
+}
+
+export interface CreateContactDto {
+  name: string;
   companyName: string;
+  phone: string;
+  type: string;
+  email: string;
+  jobPosition: string;
+  address: string;
+  internalNotes: string;
 }
