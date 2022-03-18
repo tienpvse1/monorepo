fb686cf4e2272f7df9f310f06939ee7f9f9c6040
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-03-18 14:14:48 +0700

    resolved(layout): contact details

--- client/src/components/contact/contact-info-details.tsx
+++ client/src/components/contact/contact-info-details.tsx
@@ -16,20 +16,23 @@ export const ContactInfoDetails: React.FC<ContactInfoDetailsProps> = ({
     <>
       <Row>
         <Col span={12}>
-          <MyForm label='Name'>name</MyForm>
-          <MyForm label='Email'>email</MyForm>
-          <MyForm label='Company'></MyForm>
-          <MyForm label='Birth'>
-            {/* {contact.birth
-              ? moment(contact.birth).format(BIRTH).toString()
-              : ''} */}
-              birth
-          </MyForm>
+          <MyForm label='Name'>{contact.name}</MyForm>
+          <MyForm label='Email'>{contact.email}</MyForm>
+          <div className='address-form-details'>
+            <label className='my-form-label'>Address</label>
+            <div className='my-form-content'>
+                  
+            </div>
+          </div>
         </Col>
         <Col span={12}>
-          <MyForm label='Phone'>phone</MyForm>
-          <MyForm label='Mobile'>mobile</MyForm>
-          <MyForm label='Type'></MyForm>
+          <MyForm label='Phone'>{contact.phone}</MyForm>
+          <MyForm label='Company'>{contact.company}</MyForm>
+          <MyForm label='Birth'>
+            {contact.birth
+              ? moment(contact.birth).format(BIRTH).toString()
+              : ''}
+          </MyForm>
         </Col>
       </Row>
     </>
