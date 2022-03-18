05e6d54626ff0956245797756eda2b3399e709e8
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-18 09:11:09 +0700

    resolved(layout): fix undefined cookie bug

--- client/src/pages/error.tsx
+++ client/src/pages/error.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/prop-types */
+import { Navigate } from 'react-router-dom';
 import { UnauthorizePage } from './unauthorize';
 
 interface ErrorPageProps {
@@ -7,5 +8,5 @@ interface ErrorPageProps {
 export const ErrorPage: React.FC<ErrorPageProps> = ({ error }) => {
   console.log(error);
   if (error.message == 'Unauthorize') return <UnauthorizePage />;
-  else return <div>Error occur</div>;
+  else return <Navigate to={'/login'} />;
 };
