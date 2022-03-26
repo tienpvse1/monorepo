ab0c25f4fce6685b214d6af3908f00502b5a1232
tienpvse1 <tienpvse@fpt.edu.vn>
2022-03-26 18:19:11 +0700

    resolved(bug): resolved filter bug

--- client/src/axios/index.ts
+++ client/src/axios/index.ts
@@ -18,33 +18,14 @@ export class Axios {
 
         return response.data;
       },
-      function (error) {
-        console.log(error.response.status);
+      ({ response: { status } }) => {
+        console.log(status);
         if (
-          error.response.status === StatusCodes.UNAUTHORIZED ||
-          error.response.status === StatusCodes.FORBIDDEN
+          status === StatusCodes.UNAUTHORIZED ||
+          status === StatusCodes.FORBIDDEN
         ) {
           window.location.href = `${feDomain}/login`;
         }
-
-        // // Any status codes that falls outside the range of 2xx cause this function to trigger
-        // // Do something with response error
-        // const statusCode = error.response.status;
-        // let destination = '';
-        // if (statusCode === StatusCodes.NOT_FOUND) {
-        //   destination = 'not-found';
-        // } else if (statusCode === StatusCodes.INTERNAL_SERVER_ERROR) {
-        //   destination = 'internal-server-error';
-        // } else if (
-        //   statusCode === StatusCodes.UNAUTHORIZED ||
-        //   statusCode === StatusCodes.FORBIDDEN
-        // ) {
-        //   destination = 'unauthorize';
-        // } else if (statusCode === StatusCodes.BAD_REQUEST) {
-        // } else {
-        //   destination = 'something-occur';
-        // }
-        // location.replace(`${envVars.VITE_BE_DOMAIN}/${destination}.html`);
       }
     );
   }
