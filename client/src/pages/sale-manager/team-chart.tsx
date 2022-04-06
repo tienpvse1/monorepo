9bbf11993e0b180b7a975d52b6ae1003af4e741b
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-04-07 00:21:40 +0700

    resolved(sort): sort date team chart

--- client/src/pages/sale-manager/team-chart.tsx
+++ client/src/pages/sale-manager/team-chart.tsx
@@ -2,23 +2,70 @@ import { Card, Col, DatePicker, PageHeader, Row, Tag } from "antd";
 import { useTeams } from "@modules/team/query/team.get";
 import { ColumnPlot } from "@components/chart/column-plot";
 import moment from "moment";
+import { useEffect, useState } from "react";
+import { IPipelineItem } from "@modules/pipeline-items/entity/pipeline-items.entity";
+import { dateFormat } from "@constance/date-format";
 const { RangePicker } = DatePicker;
+const { DEFAULT } = dateFormat;
 
 const TeamChart = () => {
   const { data: teams } = useTeams();
+  const [teamFilter, setTeamFilter] = useState<any>();
 
-  const teamFilter = teams?.map((team) => {
-    return {
-      id: team.id,
-      name: team.name,
-      account: team.accounts.map((account) => {
-        return {
-          salesMan: `${account.firstName} ${account.lastName}`,
-          opportunity: account.pipelineItems.length
-        }
-      })
-    }
-  })
+  const handleFilterMoment = (
+    array: IPipelineItem[],
+    beforeTime = moment().subtract(1, 'months'),
+    afterTime = moment()
+  ) => {
+    return array.filter((value) =>
+      moment(value.createdAt, DEFAULT)
+        .isBetween(moment(beforeTime, DEFAULT), moment(afterTime, DEFAULT))).length
+  }
+
+  useEffect(() => {
+    const teamFilter = teams?.map((team) => {
+      return {
+        id: team.id,
+        name: team.name,
+        account: team.accounts.map((account) => {
+          return {
+            salesMan: `${account.firstName} ${account.lastName}`,
+            opportunity: handleFilterMoment(account.pipelineItems)
+          }
+        })
+      }
+    })
+    setTeamFilter(teamFilter);
+  }, [teams])
+
+  const handleUpdateTeamData = (
+    beforeTime: moment.Moment,
+    afterTime: moment.Moment,
+    teamId: string
+  ) => {
+    const teamFilterDate = teams?.map((team) => {
+      return {
+        id: team.id,
+        name: team.name,
+        account: team.accounts.map((account) => {
+          if (team.id === teamId) {
+            return {
+              salesMan: `${account.firstName} ${account.lastName}`,
+              opportunity: handleFilterMoment(account.pipelineItems, beforeTime, afterTime)
+            }
+          } else {
+            return {
+              salesMan: `${account.firstName} ${account.lastName}`,
+              opportunity: handleFilterMoment(account.pipelineItems)
+            }
+          }
+        })
+      }
+
+    });
+
+    setTeamFilter(teamFilterDate);
+  }
 
   return (
     <div className="container-page">
@@ -51,6 +98,9 @@ const TeamChart = () => {
               extra={
                 <RangePicker
                   defaultValue={[moment().subtract(1, 'months'), moment()]}
+                  onChange={(data) => {
+                    data && handleUpdateTeamData(data[0], data[1], team.id);
+                  }}
                 />
               }
               style={{ width: 450, height: 300 }}
