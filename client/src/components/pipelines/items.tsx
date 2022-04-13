1f17e4534881d03d66e7666654cf51f82811713b
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-04-13 13:34:13 +0700

    resolved(course, filter): course detail, fitler won

--- client/src/components/pipelines/items.tsx
+++ client/src/components/pipelines/items.tsx
@@ -4,6 +4,9 @@ import { Draggable, Droppable } from 'react-beautiful-dnd';
 import { CreateCardItem } from './pipeline-items/create-card';
 import { PipelineCardItem } from './pipeline-items/card-item';
 import { ThemeColor } from '@constance/color';
+import moment from 'moment';
+import { dateFormat } from "@constance/date-format";
+const { DEFAULT } = dateFormat;
 
 interface PipelineItemsProps {
   pipelineColumn: IPipelineColumn;
@@ -21,6 +24,15 @@ export const PipelineItems: React.FC<PipelineItemsProps> = ({
   setShowCreateItemForm,
   showCreateItemForm,
 }) => {
+  const handleFilterMoment = (
+    array: IPipelineItem[],
+    beforeTime = moment().subtract(1, 'months'),
+    afterTime = moment()
+  ) => {
+    return array.filter((value) =>
+      moment(value.createdAt, DEFAULT)
+        .isBetween(moment(beforeTime, DEFAULT), moment(afterTime, DEFAULT), undefined, '[]'))
+  }
 
   return (
     <>
@@ -40,23 +52,42 @@ export const PipelineItems: React.FC<PipelineItemsProps> = ({
                 toggleClose={setShowCreateItemForm}
               />
             )}
-            {pipelineColumn.pipelineItems.map(
-              (data: IPipelineItem, index: number) => (
-                <Draggable isDragDisabled={pipelineColumn.isWon} key={data.id} draggableId={data.id} index={index}>
-                  {(provided, snapshot) => (
-                    <div
-                      className='wrapper-draggable-card'
-                      ref={provided.innerRef}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
-                    >
-                      <PipelineCardItem cardData={data} isWon={pipelineColumn.isWon}/>
-                    </div>
-                  )}
-                </Draggable>
+            {pipelineColumn.isWon ?
+              handleFilterMoment(pipelineColumn.pipelineItems).map(
+                (data: IPipelineItem, index: number) => (
+                  <Draggable isDragDisabled={pipelineColumn.isWon} key={data.id} draggableId={data.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        className='wrapper-draggable-card'
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                      >
+                        <PipelineCardItem cardData={data} isWon={pipelineColumn.isWon} />
+                      </div>
+                    )}
+                  </Draggable>
+                )
+              ) :
+              pipelineColumn.pipelineItems.map(
+                (data: IPipelineItem, index: number) => (
+                  <Draggable isDragDisabled={pipelineColumn.isWon} key={data.id} draggableId={data.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        className='wrapper-draggable-card'
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(snapshot.isDragging, provided.draggableProps.style)}
+                      >
+                        <PipelineCardItem cardData={data} isWon={pipelineColumn.isWon} />
+                      </div>
+                    )}
+                  </Draggable>
+                )
               )
-            )}
+            }
             {provided.placeholder}
           </div>
         )}
