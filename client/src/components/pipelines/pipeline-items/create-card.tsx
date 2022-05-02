185617d202b085eee35ef9d6bc3ba99fa37421f3
chuongnh4499 <nghuuchuong4499@gmail.com>
2022-05-02 12:15:51 +0700

    resolved(email template): layout

--- client/src/components/pipelines/pipeline-items/create-card.tsx
+++ client/src/components/pipelines/pipeline-items/create-card.tsx
@@ -8,7 +8,7 @@ import { Button, Card, Form, Input, message } from 'antd';
 import { FC } from 'react';
 import { useQueryClient } from 'react-query';
 import { SelectBoxGroup } from './select-box-group';
-import numberSeparator from "number-separator";
+import { templateEmailOrderInfo } from '@util/email-template';
 
 // import { dateFormat } from '@constance/date-format';
 // const { DEFAULT } = dateFormat;
@@ -75,82 +75,25 @@ export const CreateCardItem: FC<CreateCardItemProps> = ({
       onSuccess: () => {
         queryClient.refetchQueries(GET_PIPELINE_DESIGN);
         queryClient.invalidateQueries(GET_PIPELINE_DESIGN);
+        toggleClose();
+        message.success('Created successfully !')
         sendEmail({
           subject: 'VJAA CRM - Confirm Order Information',
           to: [{ email: contactEmail, isTag: false }],
-          value: `
-          <div class="invoice-box" style="
-			max-width: 800px;
-			margin: auto;
-			padding: 30px;
-      padding-top: 0px;
-			border: 1px solid #eee; 
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
-			font-size: 16px;
-			line-height: 24px;
-			font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif;
-			color: #555;
-		">
-        <h1>Order Information:</h1>
-        <table style="
-					width: 100%;
-			 		line-height: inherit;
-			 		text-align: left;
-			 		border-collapse: collapse;
-	 			">
-            <tr class="information">
-                <td colspan="2" style="padding: 5px; vertical-align: top; padding-bottom: 40px;">
-                    <table style="
-										 width: 100%;
-										 line-height: inherit;
-										 text-align: left;
-										 border-collapse: collapse;
-									 ">
-                        <tr>
-                            <td>
-                                Company name: ${companyName} <br /> Email company: ${companyEmail} <br /> City: ${companyCity}
-                            </td>
-
-                            <td>
-                                Contact name: ${contactName} <br /> Email contact: ${contactEmail}<br /> Phone: ${contactPhone}
-                            </td>
-                        </tr>
-                    </table>
-                </td>
-            </tr>
-
-            <tr class="heading" style=" background: #eee;
-            border-bottom: 1px solid #ddd;
-            font-weight: bold;">
-                <td style="padding: 5px; vertical-align: top; text-align: left;">Item</td>
-                <td style="padding: 5px; vertical-align: top; text-align: center;">Quantity</td>
-                <td style="padding: 5px; vertical-align: top; text-align: center; width: 14%;">Discount (%)</td>
-                <td style="padding: 5px; vertical-align: top; text-align: right;">Price</td>
-            </tr>
-
-            <tr class="item" style="border-bottom: 1px solid #eee;">
-                <td style="padding: 5px; vertical-align: top; text-align: left; width: 50%;">${courseName}</td>
-                <td style="padding: 5px; vertical-align: top; text-align: center;">${quantity}</td>
-                <td style="padding: 5px; vertical-align: top; text-align: center;">${discountCode * 100}%</td>
-                <td style="padding: 5px; vertical-align: top; text-align: right;">${numberSeparator(coursePrice, '.')}vnd</td>
-            </tr>
-
-            <tr class="total" style=" border-top: 2px solid #eee;
-            font-weight: bold;">
-                <td style="padding: 5px; vertical-align: top;"></td>
-                <td style="padding: 5px; vertical-align: top;"></td>
-                <td style="padding: 5px; vertical-align: top;"></td>
-                <td style="padding: 5px; vertical-align: top; text-align: right;">Total: ${numberSeparator((expectedRevenue * quantity), '.')}vnd</td>
-            </tr>
-        </table>
-    </div>
-          `
-        }, {
-          onSuccess: () => {
-            message.success('Created successfully !')
-          }
+          value: templateEmailOrderInfo(
+            'Order Information:',
+            companyName,
+            contactEmail,
+            contactName,
+            contactPhone,
+            companyEmail,
+            companyCity,
+            discountCode,
+            courseName,
+            coursePrice,
+            expectedRevenue,
+            quantity)
         })
-        toggleClose();
       },
     });
   };
